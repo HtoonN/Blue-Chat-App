b8282f814866927fa535adf423092fb67aaf5edc
HtoonN <tunnaing091500@gmail.com>
2023-11-11 23:14:37 +0700

    path

--- Controller/GetImageController.js
+++ Controller/GetImageController.js
@@ -11,12 +11,7 @@ const getImageController = async (req, res, next) => {
   if (type === "image") {
     const fileName = `${public_id.toString().split("/")[1]}.${format}`;
 
-    const fileResult = await searchFileinServer(
-      "/opt/render/project/src/File/",
-      fileName
-    );
-
-    console.log(fileResult);
+    const fileResult = await searchFileinServer("../File", fileName);
 
     if (fileResult.length) {
       sendFileToUser(res, fileName, next);

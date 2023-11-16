ae27060720667390b3f72278e74154bce3112dab
HtoonN <tunnaing091500@gmail.com>
2023-11-16 19:55:52 +0700

    update profile photo

--- Controller/UpdageUserProfileController.js
+++ Controller/UpdageUserProfileController.js
@@ -5,33 +5,39 @@ const updateUserProfileController = async (req, res) => {
   const userId = req.user.userId.toString();
   const datas = req.query;
   const result = {};
+  try {
+    if (userId) {
+      if (req.files) {
+        console.log(req.files);
+        const Imgresult = await updateProfileImageUpload(
+          req.files[0],
+          "P",
+          userId
+        );
 
-  if (userId) {
-    if (req.files) {
-      const Imgresult = await updateProfileImageUpload(
-        req.files[0],
-        "P",
-        userId
-      );
-
-      if (!Imgresult.error) {
-        datas.image = Imgresult.data;
-        result.profileImage = Imgresult.data;
+        if (!Imgresult.error) {
+          console.log(Imgresult.data);
+          datas.image = Imgresult.data;
+          result.profileImage = Imgresult.data;
+        }
       }
-    }
-    if (JSON.stringify(datas) !== "{}") {
-      result.data = await new UpdateProfileData(userId, datas).update();
 
-      if (!result.data.error) {
-        res.status(200).json(result);
+      if (JSON.stringify(datas) !== "{}") {
+        result.data = await new UpdateProfileData(userId, datas).update();
+
+        if (!result.data.error) {
+          res.status(200).json(result);
+        } else {
+          res.status(400).json(result);
+        }
       } else {
-        res.status(400).json(result);
+        res.status(200).json(result);
       }
     } else {
-      res.status(200).json(result);
+      res.status(400);
     }
-  } else {
-    res.status(400);
+  } catch (e) {
+    console.log(e);
   }
 };
 module.exports = updateUserProfileController;

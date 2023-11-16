ff1c42af1cb3fa1c3605bd5b97cf8d1e39bf3f0a
HtoonN <tunnaing091500@gmail.com>
2023-11-16 20:32:33 +0700

    multer

--- Utility/MulterUpload.js
+++ Utility/MulterUpload.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, "D:/PROJECTS/Blue Chat App/Server/File");
+    callback(null, "File");
   },
   filename: function (req, file, callback) {
     callback(null, `saveFileBlueChatApp${Date.now()}${file.originalname}`);

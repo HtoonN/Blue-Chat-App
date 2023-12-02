0d495b6a66cac20195c6b58ef418301b3c700e5c
HtoonN <tunnaing091500@gmail.com>
2023-12-02 23:29:33 +0700

    cors

--- Config/CorsConfig.js
+++ Config/CorsConfig.js
@@ -3,7 +3,7 @@ exports.corsConfig = {
     "http://localhost:3000",
     "https://bluechatapp.netlify.app",
     "https://bluechatapp1.onrender.com",
-    "https://bluechatappgit.netlify.app/",
+    "https://bluechatappgit.netlify.app",
   ],
   methods: ["GET", "POST", "DELETE", "PATCH"],
   header: "*",
